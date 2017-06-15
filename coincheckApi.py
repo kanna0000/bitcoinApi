52d283861f9422f1f4dba7cc0565d027696e95a7
kanna0000 <kanna@vivaldi.net>
2017-06-15 14:18:35 +0900

    first comment

---
+++ coincheckApi.py
@@ -0,0 +1,17 @@
+#coincheck api
+
+import json
+import requests
+
+print('what pair do you want to know?')
+data = input('please enter:')
+
+param = data
+
+def exchangeRate(param):
+    r = requests.get('https://coincheck.com/api/rate/' + param)
+    rate = json.loads(r.text)['rate']
+
+    print(rate)
+
+exchangeRate(param)
